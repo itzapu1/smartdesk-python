3ae4d8333776311d78ec59d62478d37159a2faa3
Andy Appau <andyappau@gmail.com>
2019-10-01 22:58:51 -0500

    register, login for fingerprint

--- newscreen.py
+++ newscreen.py
@@ -2,6 +2,8 @@ from tkinter import *
 import os
 import requests
 import time
+from pyfingerprint.pyfingerprint import PyFingerprint
+import RPi.GPIO as GPIO
 
 # Designing window for registration
 
@@ -68,7 +70,7 @@ def login():
     global login_screen
     login_screen = Toplevel(main_screen)
     login_screen.title("Login")
-    login_screen.geometry("350x250")
+    login_screen.geometry("350x350")
     Label(login_screen, text="Please enter details below to login").pack()
     Label(login_screen, text="").pack()
 
@@ -85,14 +87,157 @@ def login():
     username_login_entry = Entry(login_screen, textvariable=username_verify)
     username_login_entry.pack()
     Label(login_screen, text="").pack()
-    Label(login_screen, text="Password * ").pack()
+    Label(login_screen, text="Pin Number * ").pack()
     password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
     password_login_entry.pack()
     Label(login_screen, text="").pack()
     Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
+    Button(login_screen, text="onward", width=10, height=1, command=fingerprint).pack()
 
+#fingerprint screen reachable from either login or register screen
+def fingerprint():
+    global fingerprint_screen
+    fingerprint_screen = Toplevel(main_screen)
+    fingerprint_screen.title("Login")
+    fingerprint_screen.geometry("350x350")
+    Label(fingerprint_screen, text="Please scan your favorite finger").pack()
+    Label(fingerprint_screen, text="").pack()
+
+    global username_verify
+   
+    username_verify = StringVar()
+
+    global username_login_entry
+
+    Label(fingerprint_screen, text="Username * ").pack()
+    username_login_entry = Entry(fingerprint_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(fingerprint_screen, text="").pack()
+  
+    Button(fingerprint_screen, text="Register Fingerprint", width=10, height=1, command=register_fingerprint).pack()
+
+# enroll user with fingerprint function
+def register_fingerprint():
+    try:
+        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+
+        if ( f.verifyPassword() == False ):
+            raise ValueError('The given fingerprint sensor password is wrong!')
+
+    except Exception as e:
+        print('The fingerprint sensor could not be initialized!')
+        print('Exception message: ' + str(e))
+        exit(1)
+
+    ## Gets some sensor information
+    print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+
+    ## Tries to enroll new finger
+    try:
+        print('Waiting for finger...')
+
+        ## Wait that finger is read
+        while ( f.readImage() == False ):
+            pass
+
+        ## Converts read image to characteristics and stores it in charbuffer 1
+        f.convertImage(0x01)
+
+        ## Checks if finger is already enrolled
+        result = f.searchTemplate()
+        positionNumber = result[0]
+
+        if ( positionNumber >= 0 ):
+            print('Template already exists at position #' + str(positionNumber))
+            exit(0)
+
+        print('Remove finger...')
+        time.sleep(2)
+
+        print('Waiting for same finger again...')
+
+        ## Wait that finger is read again
+        while ( f.readImage() == False ):
+            pass
+
+        ## Converts read image to characteristics and stores it in charbuffer 2
+        f.convertImage(0x02)
+
+        ## Compares the charbuffers
+        if ( f.compareCharacteristics() == 0 ):
+            raise Exception('Fingers do not match')
+
+        ## Creates a template
+        f.createTemplate()
+
+        ## Saves template at new position number
+        positionNumber = f.storeTemplate()
+        print('Finger enrolled successfully!')
+        print('New template position #' + str(positionNumber))
+
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        exit(1)
 
 # Implementing event on register button
+
+def fingerprint_login():
+    try:
+        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+
+        if ( f.verifyPassword() == False ):
+            raise ValueError('The given fingerprint sensor password is wrong!')
+
+    except Exception as e:
+        print('The fingerprint sensor could not be initialized!')
+        print('Exception message: ' + str(e))
+        exit(1)
+
+    ## Gets some sensor information
+    print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+
+    ## Tries to search the finger and calculate hash
+    try:
+        print('Waiting for finger...')
+
+        ## Wait that finger is read
+        while ( f.readImage() == False ):
+            pass
+
+        ## Converts read image to characteristics and stores it in charbuffer 1
+        f.convertImage(0x01)
+
+        ## Searchs template
+        result = f.searchTemplate()
+
+        positionNumber = result[0]
+        accuracyScore = result[1]
+
+        if ( positionNumber == -1 ):
+            print('No match found!')
+            exit(0)
+        else:
+            print('Found template at position #' + str(positionNumber))
+            print('The accuracy score is: ' + str(accuracyScore))
+
+        ## OPTIONAL stuff
+        ##
+
+        ## Loads the found template to charbuffer 1
+        f.loadTemplate(positionNumber, 0x01)
+
+        ## Downloads the characteristics of template loaded in charbuffer 1
+        characterics = str(f.downloadCharacteristics(0x01)).encode('utf-8')
+
+        ## Hashes characteristics of template
+        print('SHA-2 hash of template: ' + hashlib.sha256(characterics).hexdigest())
+
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        exit(1)
+
 
 def register_user():
     username_info = username.get()
@@ -130,7 +275,7 @@ def next():
     global Suggestions_screen
     Suggestions_screen = Toplevel(main_screen)
     Suggestions_screen.title("Suggestions")
-    Suggestions_screen.geometry("350x250")
+    Suggestions_screen.geometry("350x350")
     Label(Suggestions_screen, text="Here are our suggestions").pack()
     Label(Suggestions_screen, text="").pack()
 
@@ -192,7 +337,7 @@ def next():
     
         Timer_screen = Toplevel(main_screen)
         Timer_screen.title("Timer Option")
-        Timer_screen.geometry("350x250")
+        Timer_screen.geometry("350x350")
 
         Label(Timer_screen, text="Please choose a preferred timer option").pack()
         Label(Timer_screen, text="").pack()
@@ -236,6 +381,13 @@ def next_3():
 def login_verify():
     username1 = username_verify.get()
     password1 = password_verify.get()
+
+    print(password1)
+
+    payload1 = { 'username': str(username1), 'pin_number': str(password1) }
+    r = requests.post('http://localhost:3003/login', json=payload1)
+    print(r.json)
+
     username_login_entry.delete(0, END)
     password_login_entry.delete(0, END)
 
@@ -305,7 +457,7 @@ def delete_user_not_found_screen():
 def main_account_screen():
     global main_screen
     main_screen = Tk()
-    main_screen.geometry("300x250")
+    main_screen.geometry("300x350")
     main_screen.title("Smart Desk")
     Label(text="The Vault Login", bg="grey", width="50", height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
